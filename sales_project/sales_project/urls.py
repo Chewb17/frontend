91e4bd28a72528ae8ef7c95770c0a54202911a54
Lucas Coelho de Moraes <110632462+Chewb17@users.noreply.github.com>
2025-05-07 15:33:01 -0300

    Delete sales_project directory

--- sales_project/sales_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('sales.urls')),  # Inclua as URLs do app sales
-]
